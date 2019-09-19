231ec8171f9bfd2690d9255ba0a1b35f749e724f
unknown <shklyarigor2015@gmail.com>
2019-09-19 15:03:18 +0300

    Updates

---
+++ precache-manifest.3e4d30c20973b063a007a70ebfb6dac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa3b7918784cf2cce7ac491b0430263",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "a273c99782168491c333",
+    "url": "/react_add-todo-form/static/css/main.0c8b1b03.chunk.css"
+  },
+  {
+    "revision": "6c8f1d725de506c0f583",
+    "url": "/react_add-todo-form/static/js/2.e8aa7888.chunk.js"
+  },
+  {
+    "revision": "a273c99782168491c333",
+    "url": "/react_add-todo-form/static/js/main.4e5042e0.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
